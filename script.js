d10e5155538bf0f3ca58dede2be0daf77cef8be9
Dvrs-1 <davariusburton9@gmail.com>
2025-12-08 14:07:07 -0600

    fromthebutton

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log('Hello!');
-document.write('hello world')
+document.write('hello stackblitz world')
+
+const addTask = document.getElementById('add-list-item');
+
+addTask.addEventListener('click', addTaskToList)
+
+function addTaskToList() {
+    document.write('the button was clicked')
+}
